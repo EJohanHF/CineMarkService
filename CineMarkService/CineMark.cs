46f0e5ce1f78b084e6ed83ea4472bc86481def63
EJOHANHF <e.johan.herrera.f@gmail.com>
2023-08-19 22:02:25 -0500

    test

--- CineMarkService/CineMark.cs
+++ CineMarkService/CineMark.cs
@@ -9,7 +9,7 @@ using CineMarkModelo;
 
 namespace CineMarkService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    // test
     public class CineMark : ICineMark
     {
         LNActore lnActore = new LNActore();
